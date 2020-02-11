20eb0d06076c4abf0d3b7010f4a71bb697aff2d0
cyenciso <48849730+cyenciso@users.noreply.github.com>
2020-02-10 19:19:38 -0600

    basic GUI structure

---
+++ LeapLog/Journal.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LeapLog
+{
+    /// <summary>
+    /// Go ahead and write a summary for Journal.xaml right here!
+    /// </summary>
+    public partial class Journal : UserControl
+    {
+        public Journal()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- LeapLog/MainWindow.xaml.cs
+++ LeapLog/MainWindow.xaml.cs
@@ -16,13 +16,37 @@ using System.Windows.Shapes;
 namespace LeapLog
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// The main window represents is used to navigate between User Controls.
+    /// Each button makes a different section of our program visible.
     /// </summary>
+   
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //Button Click Events
+        private void journalBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SetActiveUserControl(journalUserControl);
+        }
+
+        private void tAccBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SetActiveUserControl(t_AccountsUserControl);
+        }
+
+        public void SetActiveUserControl(UserControl control)
+        {
+            //make all user control sections invisible and collapsed
+            journalUserControl.Visibility = Visibility.Collapsed;
+            t_AccountsUserControl.Visibility = Visibility.Collapsed;
+
+            //make the correct user control section visible
+            control.Visibility = Visibility.Visible;
         }
     }
 }
