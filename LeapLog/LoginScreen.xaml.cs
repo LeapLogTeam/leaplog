4d01683ef4c699cea5bc0c01db9b3e672806845a
jdhammer-coder <60295314+jdhammer-coder@users.noreply.github.com>
2020-02-18 18:26:26 -0600

    this is a basic login screen with basic funtionalities

---
+++ LeapLog/LoginScreen.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace LeapLog
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public object MessageBoxIcon { get; private set; }
+
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (UserName.Text == "leaplog" && Pass.Text == "leaplog")
+            {
+                MainWindow MS = new MainWindow();
+                MS.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show(this, "Wrong User Name or Password", "Information");//, MessageBoxButtons.OK,
+               // MessageBoxIcon.Information);
+                UserName.Clear();
+                Pass.Clear();
+                UserName.Focus();
+            }
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
