9bca2d7402665009566ac52b3cd0d9c9456d08b8
Jason174 <60452860+Jason174@users.noreply.github.com>
2020-02-18 23:47:51 -0600

    Add t-account basics

---
+++ LeapLog/Database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeapLog
+{
+     public static class Database
+     {
+          private static List<Entry> entries;
+
+          internal static List<Entry> Entries { get => entries; set => entries = value; }
+
+          static Database()
+          {
+               entries = new List<Entry>();
+          }
+     }
+}
