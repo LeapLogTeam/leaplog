94833055d71a79f1632a9770fe8a8cfe1a064571
jdhammer-coder <60295314+jdhammer-coder@users.noreply.github.com>
2020-03-04 18:26:41 -0600

    update for login screen

--- leaplog/LoginScreen.xaml.cs
+++ leaplog/LoginScreen.xaml.cs
@@ -26,13 +26,10 @@ namespace LeapLog
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
+        //enter button
         private void button_Click(object sender, RoutedEventArgs e)
         {
+            
             //updated textbox to passwordbox 
             //database funtionality coming soon
             if (UserName.Text == "leaplog" && Pass.Password == "leaplog")
@@ -51,14 +48,119 @@ namespace LeapLog
             }
         }
 
-        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        //Enter button funtionality for enter button
+        private void enterButton_KeyUp(object sender, KeyEventArgs e)
         {
+             
+            
+
+                if (e.Key == Key.Enter)
+                {
+                     
+                    //our code here
+                    if (UserName.Text == "leaplog" && Pass.Password == "leaplog")
+                    {
+                        MainWindow MS = new MainWindow();
+                        MS.Show();
+                        this.Hide();
+                    }
+
+                    else 
+                    {
+                        MessageBox.Show(this, "Wrong User Name or Password.");//, MessageBoxButtons.OK,
+                                                                                             
+                        UserName.Clear();
+                        Pass.Clear();
+                        UserName.Focus();
+                        
+                    }
+                   
+                }
+            
+
 
         }
+ 
+        //enter button functionality for pass box
+        private void Pass_KeyUp_1(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
 
+                //our code here
+                if (UserName.Text == "leaplog" && Pass.Password == "leaplog")
+                {
+                    MainWindow MS = new MainWindow();
+                    MS.Show();
+                    this.Hide();
+                }
+
+                else
+                {
+                    MessageBox.Show(this, "Wrong User Name or Password.");//, MessageBoxButtons.OK,
+
+                    UserName.Clear();
+                    Pass.Clear();
+                    UserName.Focus();
+
+                }
+
+            }
+        }
+
+
+        //clear the box when mouse clicks
+        private void UserName_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            UserName.Clear();
+        }
+
+        //clear the box when mouse clicks
+        private void Pass_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Pass.Clear();
+        }
+
+
+        //clear textbox when tab is pressed
+        private void UserName_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Tab)
+            {
+                //my code here
+                UserName.Clear();
+                 
+            }
+        }
+        //clear textbox when tab is pressed
+        private void Pass_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Tab)
+            {
+                //my code here
+                Pass.Clear();
+                 
+            }
+        }
+
+        //exit button
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
+
+
+        //close the window when esc is pressed
+        private void Window_KeyUp_1(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                //our code here
+                this.Close();
+
+            }
+        }
+
+        
     }
 }
