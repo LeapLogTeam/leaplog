0d5bdecfdcd67662eb3f3b8119dcb899fbe3f27a
jdhammer-coder <60295314+jdhammer-coder@users.noreply.github.com>
2020-04-18 21:56:42 -0500

    Added user window

---
+++ LeapLog/AddNewUser.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LeapLog
+{
+    /// <summary>
+    /// Interaction logic for AddNewUser.xaml
+    /// </summary>
+    public partial class AddNewUser : Window
+    {
+        public AddNewUser()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void exitButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            // this.Close();
+            //Process.GetCurrentProcess().Close();
+            Environment.Exit(Environment.ExitCode);
+
+
+
+
+        }
+
+        private void backToLogin_Click(object sender, RoutedEventArgs e)
+        {
+            LoginScreen MS = new LoginScreen();
+            MS.Show();
+            this.Hide();
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                //our code here
+                // this.Close();
+                Environment.Exit(Environment.ExitCode);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void createButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginManager sqlTables = new LoginManager();
+
+            string messageBoxText = "Username and password fields cannot be null or empty.";
+            string caption = "Wrong Input";
+            MessageBoxButton button = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+
+            //<<------- this chooses the table where the data will be added to-------->>
+            string username = newUsername.Text.Replace(" ", "");
+            string password = newPass.Password.ToString();
+
+            if (String.IsNullOrEmpty(username) || username == "")
+            {
+                MessageBox.Show(messageBoxText, caption, button, icon);
+            }
+            else if (String.IsNullOrEmpty(password) || password == "")
+            {
+                MessageBox.Show(messageBoxText, caption, button, icon);
+            }
+            else
+                sqlTables.WriteData("INSERT INTO LoginTable VALUES ('" + username + "','" + password + "')");
+
+            newUsername.Clear();
+            newPass.Clear();
+
+        }
+    }
+}
