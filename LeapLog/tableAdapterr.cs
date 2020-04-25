96a7217f3e30fc8df829238f515e541998f49c86
jdhammer-coder <60295314+jdhammer-coder@users.noreply.github.com>
2020-04-24 19:39:37 -0500

    Excel Export and Automatic database update path
    -Excel Export
    -Automatic database update path

---
+++ LeapLog/tableAdapterr.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeapLog
+{
+    class tableAdapterr
+    {
+     //Journal table
+        public int ID{ get; set; }
+        public string Date { get; set; }
+        public string Account_1 { get; set; }
+        public string Account_2 { get; set; }
+        public string Type_1 { get; set; }
+        public string Type_2 { get; set; }
+        public int Debit { get; set; }
+        public int Credit { get; set; }
+    }
+}
